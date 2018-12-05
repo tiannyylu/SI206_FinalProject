1033e6cf31bcce90533715891b520e20104aecc2
Tianny Lu <tiannyylu@gmail.com>
2018-12-04 20:35:56 -0500

    add to database function added

--- final_project.py
+++ final_project.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import json
+import sqlite3
 from datetime import date
 import spotify_info
 import spotipy
@@ -24,9 +25,7 @@ except:
     cacheDict = {}
 
 #get albums data
-def get_album(id):
-    today = str(date.today())
-    k = id + '(' + today +')' #album ID and the current date
+def get_album(k, id):
     if k in cacheDict:
         print('data is already in cache')
         return cacheDict[k]
@@ -38,19 +37,39 @@ def get_album(id):
             dumped_json_cache = json.dumps(cacheDict)
             file_write = open(fname,"w")
             file_write.write(dumped_json_cache)
-            file_write.close() # Close the open file
+            file_write.close()
+            albumsLst.append(cacheDict) # Close the open file
             return cacheDict[k]
         except:
             print("Wasn't in cache and wasn't valid search either")
             return None
 
-def addToTable(spotifyList, conn, cur):
+#creating Albums table
+def setupTable(cur):
+    cur.execute('CREATE TABLE Albums (album TEXT, artist_name TEXT, popularity_score INTEGER, total_tracks INTEGER, release_date TEXT)')
+
+def addtoTable(spotifyList, conn, cur):
+    for album in spotifyList:
+        albumName = album['name']
+        artist = album['artists'][0]['name']
+        popularity = album['popularity']
+        total_tracks = album['total_tracks']
+        release_date = album['release_date']
+        cur.execute('INSERT INTO Albums (album, artist_name, popularity_score, total_tracks, release_date) VALUES (?, ?, ?, ?, ?)', (albumName, artist, popularity, total_tracks, release_date))
+    conn.commit()
     pass
 
 
 
 albumIDLst = ['3CKVXhODttZebJAzjUs2un','6zk4RKl6JFlgLCV4Z7DQ7N','61ulfFSmmxMhc2wCdmdMkN','5M8U1qYKvRQHJJVHmPY7QD','0ny6mZMBrYSO0s8HAKbcVq','3cr4Xgz8nnfp7iYbVqwzzH','6uIB97CqMcssTss9WrtX8c']
+spotifyList = []
 for id in albumIDLst:
-    result = get_album(id)
-    print(result['popularity'])
-    print(result['release_date'])
+    today = str(date.today())
+    k = id + '(' + today +')' #album ID and the current date
+    result = get_album(k, id)
+    spotifyList.append(result)
+
+conn = sqlite3.connect('/Users/tiannyylu/Desktop/206_programs/final-project-tiannyylu/albums.sqlite')
+cur = conn.cursor()
+setupTable(cur)
+addtoTable(spotifyList, conn, cur)
