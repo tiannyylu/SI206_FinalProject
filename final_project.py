9c4c479b87a9f782aa48e086d3ea7b13c20f7273
Tianny Lu <tiannyylu@gmail.com>
2018-12-05 23:02:19 -0500

    get_album actually fixed and working

--- final_project.py
+++ final_project.py
@@ -16,25 +16,21 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
 #get albums data
-def get_SpotifyLst(albumIDLst):
-    spotifyList = [] # contains a list of all the album dictionaries which includes the track list for each album
+def get_album(id):
     albumdict = {} # contains album name (key, value) and a list of track dictionaries under the key 'tracks'
     trackList = [] # a list of track dictionaries
-    for id in albumIDLst:
-        albumResults = sp.album(id)
-        albumName = albumResults['name']
-        for track in albumResults['tracks']['items']: # looping through the list of tracks of the album object
-            trackdict = {} # a dictionary for a single track
-            trackdict['artist_name'] = track['artists'][0]['name']
-            trackdict['track_name'] = track['name']
-            trackdict['track_num'] = track['track_number']
-            trackdict['track_lengthMS'] = track['duration_ms']
-            trackList.append(trackdict)
-        albumdict['album_name'] = albumName
-        albumdict['tracks'] = trackList
-    spotifyList.append(albumdict)
-    return spotifyList
-
+    albumResults = sp.album(id)
+    albumName = albumResults['name']
+    for track in albumResults['tracks']['items']:
+        trackdict = {} # a dictionary for a single track
+        trackdict['artist_name'] = track['artists'][0]['name']
+        trackdict['track_name'] = track['name']
+        trackdict['track_num'] = track['track_number']
+        trackdict['track_lengthMS'] = track['duration_ms']
+        trackList.append(trackdict)
+    albumdict['album_name'] = albumName
+    albumdict['tracks'] = trackList
+    return albumdict
 
 def addtoTable(spotifyList, conn, cur):
     cur.execute('CREATE TABLE IF NOT EXISTS Albums (album TEXT, artist_name TEXT, track TEXT, track_number INTEGER, length_of_track INTEGER)')
@@ -52,9 +48,12 @@ def addtoTable(spotifyList, conn, cur):
 
 
 albumIDLst = ['3CKVXhODttZebJAzjUs2un','6zk4RKl6JFlgLCV4Z7DQ7N','61ulfFSmmxMhc2wCdmdMkN','5M8U1qYKvRQHJJVHmPY7QD','0ny6mZMBrYSO0s8HAKbcVq','3cr4Xgz8nnfp7iYbVqwzzH','6uIB97CqMcssTss9WrtX8c','7DuJYWu66RPdcekF5TuZ7w']
-results = get_SpotifyLst(albumIDLst)
+spotifyList = []
+for id in albumIDLst:
+    results = get_album(id)
+    spotifyList.append(results)
 
 conn = sqlite3.connect('/Users/tiannyylu/Desktop/206_programs/final-project-tiannyylu/albums.sqlite')
 cur = conn.cursor()
 
-addtoTable(results, conn, cur)
+addtoTable(spotifyList, conn, cur)
