0512f4b7db015a1384abd6c209abadf919a6571b
Tianny Lu <tiannyylu@gmail.com>
2018-12-13 15:37:52 -0500

    tests added

--- final_project.py
+++ final_project.py
@@ -19,6 +19,11 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 #get albums data
 def get_album(id):
+    """ Uses an album id and the Spotify API to search for objects and
+    parses through the results to create a new dictionary for each track
+
+    id - spotify album id number
+    """
     albumdict = {} # contains album name (key, value) and a list of track dictionaries under the key 'tracks'
     trackList = [] # a list of track dictionaries
     albumResults = sp.album(id)
@@ -34,7 +39,26 @@ def get_album(id):
     albumdict['tracks'] = trackList
     return albumdict
 
+def getSpotifyList(idLst):
+    """ Creates a list of the album dictionaries with the keys, values from the get_album functions
+
+    idList - a list of album IDs
+    """
+    spotifyList = []
+    for id in idLst:
+        results = get_album(id)
+        spotifyList.append(results)
+    return spotifyList
+
 def addtoTable(spotifyList, conn, cur):
+    """ Checks if there is a database called Albums and if not, then a new table is created
+    with columns (album, artists_name, track, track_number, length_of_track). Then, the function
+    iterates through the list of dictionaries and adds each element to the table.
+
+    spotifyList - list of dictionaries for each album
+    conn - connector
+    cur - cursor to database
+    """
     cur.execute('CREATE TABLE IF NOT EXISTS Albums (album TEXT, artist_name TEXT, track TEXT, track_number INTEGER, length_of_track INTEGER)')
     for album in spotifyList:
         albumName = album['album_name']
@@ -47,7 +71,13 @@ def addtoTable(spotifyList, conn, cur):
     conn.commit()
     pass
 
-def getTrackLengths(cur):
+def getTrackLengths(cur, spotifyList):
+    """ Selects length of each track in the album and converts that time from milliseconds to minutes.
+    Finds the average length of a song per album and adds the album name and average length
+    to a dictionary and then writes the dictionary into a CSV file
+
+    cur - cursor to database
+    """
     avgDict= {}
     albumNames = [item['album_name'] for item in spotifyList]
     for name in albumNames:
@@ -70,8 +100,11 @@ def getTrackLengths(cur):
     return avgDict
 
 def drawBarChart(avgDict):
+    """ Uses the dictionary of average length of a track for each albums and plots those values in a bar chart
+
+    avgDict - a dictionary of album names as the keys and the average track length as the values
+    """
     names = list(avgDict.keys())
-    print(names)
     nums = list(avgDict.values())
     fig = plt.figure(figsize=(13,16))
     ax = fig.add_subplot(111)
@@ -91,16 +124,46 @@ def drawBarChart(avgDict):
     fig.savefig('SongLengths.png')
 
 
-conn = sqlite3.connect('/Users/tiannyylu/Desktop/206_programs/final-project-tiannyylu/albums.sqlite')
-cur = conn.cursor()
-
-albumIDLst = ['3CKVXhODttZebJAzjUs2un','6zk4RKl6JFlgLCV4Z7DQ7N','61ulfFSmmxMhc2wCdmdMkN','5M8U1qYKvRQHJJVHmPY7QD','0ny6mZMBrYSO0s8HAKbcVq','3cr4Xgz8nnfp7iYbVqwzzH','6uIB97CqMcssTss9WrtX8c','7DuJYWu66RPdcekF5TuZ7w']
-spotifyList = []
-for id in albumIDLst:
-    results = get_album(id)
-    spotifyList.append(results)
-
+# conn = sqlite3.connect('/Users/tiannyylu/Desktop/206_programs/final-project-tiannyylu/albums.sqlite')
+# cur = conn.cursor()
+#
+# albumIDLst = ['3CKVXhODttZebJAzjUs2un','6zk4RKl6JFlgLCV4Z7DQ7N','61ulfFSmmxMhc2wCdmdMkN','5M8U1qYKvRQHJJVHmPY7QD','0ny6mZMBrYSO0s8HAKbcVq','3cr4Xgz8nnfp7iYbVqwzzH','6uIB97CqMcssTss9WrtX8c','7DuJYWu66RPdcekF5TuZ7w']
+# spotifyList = getSpotifyList(albumIDLst)
+#
 # addtoTable(spotifyList, conn, cur)
+#
+# avgDict = getTrackLengths(cur)
+# print(avgDict['Merry Christmas'])
+# drawBarChart(avgDict)
 
-avgDict = getTrackLengths(cur)
-drawBarChart(avgDict)
+class TestProject(unittest.TestCase):
+    def setUp(self):
+        clientID = spotify_info.SPOTIFY_CLIENT_ID
+        clientSecret = spotify_info.SPOTIFY_CLIENT_SECRET
+        redirectURI = spotify_info.SPOTIFY_REDIRECT_URI
+
+        client_credentials_manager = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+        self.conn = sqlite3.connect('/Users/tiannyylu/Desktop/206_programs/final-project-tiannyylu/albums.sqlite')
+        self.cur = self.conn.cursor()
+
+        albumIDLst = ['3CKVXhODttZebJAzjUs2un','6zk4RKl6JFlgLCV4Z7DQ7N','61ulfFSmmxMhc2wCdmdMkN','5M8U1qYKvRQHJJVHmPY7QD','0ny6mZMBrYSO0s8HAKbcVq','3cr4Xgz8nnfp7iYbVqwzzH','6uIB97CqMcssTss9WrtX8c','7DuJYWu66RPdcekF5TuZ7w']
+        self.spotifyList = getSpotifyList(albumIDLst)
+        addtoTable(self.spotifyList, self.conn, self.cur)
+
+    def test_addtoTable(self):
+        self.cur.execute('SELECT * FROM Albums')
+        self.assertEqual(103, len(self.cur.fetchall()))
+
+    def test_getTrackLengths(self):
+        avgDict = getTrackLengths(self.cur, self.spotifyList)
+        self.assertEqual(avgDict['Merry Christmas'], 3.8)
+
+    def tearDown(self):
+        self.conn.close()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+
+TestProject().tearDown()
